8c1f739bfbf70dc15eabee488521e1c04e977328
Devin Arnold <dbarnold321@gmail.com>
2019-05-12 23:45:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |x|
   if   cheese_types.include? (x)
-    return item
+    return x
 end
 end
 return nil
